b180164ed0baf406a58e268890401fdcec52f118
Milan <milanvpage@gmail.com>
2021-03-21 18:21:51 -0700

    commit tree 84a7f769fc388875e77b270d993deed2bb6e44c3

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,33 @@
+class Genre
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        genre_artists = []
+        Song.all.each do |song| 
+          if song.genre == self
+            genre_artists << song.artist
+          end
+        end
+        genre_artists
+    end
+
+        
+
+end
+
 
